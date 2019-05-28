2333c61ef9830273c90b23c1a407c8bc0fb3ee0c
huichen <huichen@ubuntu16.04>
2019-05-28 14:02:53 +0800

    ex5.17

--- chapter5/ex5.16.cpp
+++ chapter5/ex5.16.cpp
@@ -2,9 +2,28 @@
 #include <vector>
 using namespace std;
 
+bool isPrefix(vector<int> const& obj1, vector<int> const& obj2)
+{
+    if (obj1.size() > obj2.size())
+        isPrefix(obj2, obj1);
+    
+    auto beg1 = obj1.begin(), beg2 = obj2.begin();
+    while (beg1 != obj1.end())
+    {
+        if (*beg1 != *beg2)
+            return false;
+        beg1++;
+        beg2++;
+    }
+    return true;
+}
+
 int main()
 {
-    cout << "OK" << endl;
+    vector<int> obj1 = {0, 1, 1, 2};
+    vector<int> obj2 = {0, 1, 1, 2, 3, 5, 8};
+
+    cout << (isPrefix(obj1, obj2) ? "yes" : "no") << endl;
 
     return 0;
 }
